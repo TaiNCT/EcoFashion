38a169f57c9ef34f6c9908ed454ad01df69fac0f
tthaingoc <artuan.thaingoc@gmail.com>
2025-09-24 13:47:24 +0700

    [Address-checkout-mobile] config

--- EcoFashionBackEnd/EcoFashionBackEnd/Migrations/20250923001833_FixUserAddress.Designer.cs
+++ EcoFashionBackEnd/EcoFashionBackEnd/Migrations/20250923001833_FixUserAddress.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EcoFashionBackEnd.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EcoFashionBackEnd.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250923001833_FixUserAddress")]
+    partial class FixUserAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
